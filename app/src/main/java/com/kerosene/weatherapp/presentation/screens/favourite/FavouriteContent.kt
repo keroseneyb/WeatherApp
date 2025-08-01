08669df30a3c57ce354346a993626759854cae00
keroseneyb <keroseneyb@gmail.com>
2025-08-01 14:49:42 +0500

    Add content functions

--- app/src/main/java/com/kerosene/weatherapp/presentation/screens/favourite/FavouriteContent.kt
+++ app/src/main/java/com/kerosene/weatherapp/presentation/screens/favourite/FavouriteContent.kt
@@ -1,3 +1,9 @@
 package com.kerosene.weatherapp.presentation.screens.favourite
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavouriteContent(component: FavouriteComponent) {
+
+}
 
