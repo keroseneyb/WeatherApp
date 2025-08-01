a994b33b1c7dcdfdf4f8239bd41d65d1cff22114
keroseneyb <keroseneyb@gmail.com>
2025-08-01 14:45:53 +0500

    Add realization root component

--- app/src/main/java/com/kerosene/weatherapp/presentation/screens/root/DefaultRootComponent.kt
+++ app/src/main/java/com/kerosene/weatherapp/presentation/screens/root/DefaultRootComponent.kt
@@ -1,7 +1,107 @@
 package com.kerosene.weatherapp.presentation.screens.root
 
+import android.os.Parcelable
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.push
+import com.arkivanov.decompose.value.Value
+import com.kerosene.weatherapp.domain.entity.City
+import com.kerosene.weatherapp.presentation.screens.details.DefaultDetailsComponent
+import com.kerosene.weatherapp.presentation.screens.favourite.DefaultFavouriteComponent
+import com.kerosene.weatherapp.presentation.screens.search.DefaultSearchComponent
+import com.kerosene.weatherapp.presentation.screens.search.OpenReason
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.parcelize.Parcelize
 
-class DefaultRootComponent(
-    componentContext: ComponentContext,
-) : RootComponent, ComponentContext by componentContext
+class DefaultRootComponent @AssistedInject constructor(
+    private val detailsComponentFactory: DefaultDetailsComponent.Factory,
+    private val favouriteComponentFactory: DefaultFavouriteComponent.Factory,
+    private val searchComponentFactory: DefaultSearchComponent.Factory,
+    @Assisted("componentContext") componentContext: ComponentContext,
+) : RootComponent, ComponentContext by componentContext {
+
+    private val navigationStack = StackNavigation<Config>()
+
+    override val stack: Value<ChildStack<*, RootComponent.Child>> = childStack(
+        source = navigationStack,
+        initialConfiguration = Config.Favourite,
+        handleBackButton = true,
+        childFactory = ::child
+    )
+
+    private fun child(
+        config: Config,
+        componentContext: ComponentContext,
+    ): RootComponent.Child {
+        return when (config) {
+            is Config.Details -> {
+                val component = detailsComponentFactory.create(
+                    city = config.city,
+                    onBackClicked = {
+                        navigationStack.pop()
+                    },
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Details(component)
+            }
+
+            Config.Favourite -> {
+                val component = favouriteComponentFactory.create(
+                    onCityItemClicked = {
+                        navigationStack.push(Config.Details(it))
+                    },
+                    onAddFavouriteClicked = {
+                        navigationStack.push(Config.Search(OpenReason.AddToFavourite))
+                    },
+                    onSearchClicked = {
+                        navigationStack.push(Config.Search(OpenReason.RegularSearch))
+                    },
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Favourite(component)
+            }
+
+            is Config.Search -> {
+                val component = searchComponentFactory.create(
+                    openReason = config.openReason,
+                    onBackClicked = {
+                        navigationStack.pop()
+                    },
+                    onCitySavedToFavourite = {
+                        navigationStack.pop()
+                    },
+                    onForecastForCityRequested = {
+                        navigationStack.push(Config.Details(it))
+                    },
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Search(component)
+            }
+        }
+    }
+
+    sealed interface Config : Parcelable {
+
+        @Parcelize
+        data object Favourite : Config
+
+        @Parcelize
+        data class Search(val openReason: OpenReason) : Config
+
+        @Parcelize
+        data class Details(val city: City) : Config
+    }
+
+    @AssistedFactory
+    interface Factory {
+
+        fun create(
+            @Assisted("componentContext") componentContext: ComponentContext,
+        ): DefaultRootComponent
+    }
+}
--- app/src/main/java/com/kerosene/weatherapp/presentation/screens/root/RootComponent.kt
+++ app/src/main/java/com/kerosene/weatherapp/presentation/screens/root/RootComponent.kt
@@ -1,4 +1,21 @@
 package com.kerosene.weatherapp.presentation.screens.root
 
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.kerosene.weatherapp.presentation.screens.details.DetailsComponent
+import com.kerosene.weatherapp.presentation.screens.favourite.FavouriteComponent
+import com.kerosene.weatherapp.presentation.screens.search.SearchComponent
+
 interface RootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+
+    sealed interface Child {
+
+        data class Favourite(val component: FavouriteComponent) : Child
+
+        data class Search(val component: SearchComponent) : Child
+
+        data class Details(val component: DetailsComponent) : Child
+    }
 }
